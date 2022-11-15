eccd79d4258ec6c0d87bc4ab7ab08887591e3e67
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-15 15:35:51 +0100

    Base project

--- src/main/java/pl/training/shop/commons/aop/TransactionProvider.java
+++ src/main/java/pl/training/shop/commons/aop/TransactionProvider.java
@@ -4,10 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import pl.training.shop.commons.Annotations;
 
 @Aspect
 @Component
@@ -18,7 +18,7 @@ public class TransactionProvider {
 
     @Around("@annotation(pl.training.shop.commons.aop.Atomic) || within(@pl.training.shop.commons.aop.Atomic *)")
     public Object runWithTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
-        var atomic = getAtomicAnnotation(joinPoint);
+        var atomic = Annotations.findAnnotation(joinPoint, Atomic.class);
         var transactionDefinition = getTransactionDefinition(atomic);
         var transaction = transactionManager.getTransaction(transactionDefinition);
         try {
@@ -29,15 +29,6 @@ public class TransactionProvider {
             transaction.setRollbackOnly();
             throw throwable;
         }
-    }
-
-    private Atomic getAtomicAnnotation(ProceedingJoinPoint joinPoint) throws NoSuchMethodException {
-        var classAnnotation = joinPoint.getTarget().getClass().getAnnotation(Atomic.class);
-        var signature = (MethodSignature) joinPoint.getSignature();
-        var methodName = signature.getMethod().getName();
-        var parameterTypes = signature.getMethod().getParameterTypes();
-        var methodAnnotation = joinPoint.getTarget().getClass().getMethod(methodName, parameterTypes).getAnnotation(Atomic.class);
-        return methodAnnotation != null ? methodAnnotation : classAnnotation;
     }
 
     private DefaultTransactionDefinition getTransactionDefinition(Atomic atomic) {

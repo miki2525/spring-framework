3c07678fbb482cf914daae4be7426f23485b3a43
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-15 15:59:08 +0100

    Base project

--- src/main/java/pl/training/shop/commons/Annotations.java
+++ src/main/java/pl/training/shop/commons/Annotations.java
@@ -6,18 +6,20 @@ import org.aspectj.lang.reflect.MethodSignature;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Optional;
 
 public class Annotations {
 
-    public static  <T extends Annotation> T getClassAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) {
+    public static <T extends Annotation> T getClassAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) {
         return joinPoint.getTarget().getClass().getAnnotation(type);
     }
 
-    public static  <T extends Annotation> T getMethodAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) throws NoSuchMethodException {
+    public static <T extends Annotation> T getMethodAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) throws NoSuchMethodException {
         return getTargetMethod(joinPoint).getAnnotation(type);
     }
 
-    public static  <T extends Annotation> T findAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) throws NoSuchMethodException {
+    public static <T extends Annotation> T findAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) throws NoSuchMethodException {
         var annotation = getClassAnnotation(joinPoint, type);
         return annotation != null ? annotation : getMethodAnnotation(joinPoint, type);
     }
@@ -27,6 +29,13 @@ public class Annotations {
         var methodName = signature.getMethod().getName();
         var parameterTypes = signature.getMethod().getParameterTypes();
         return joinPoint.getTarget().getClass().getMethod(methodName, parameterTypes);
+    }
+
+    public static <T extends Annotation> Optional<T> findAnnotation(Annotation[] annotations, Class<T> type) {
+        return Arrays.stream(annotations)
+                .filter(type::isInstance)
+                .map(type::cast)
+                .findFirst();
     }
 
 }

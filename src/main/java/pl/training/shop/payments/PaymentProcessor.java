608c2a815d3e752f3d1a81cf27bdb5c4327104e0
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-16 15:06:29 +0100

    Base project

--- src/main/java/pl/training/shop/payments/PaymentProcessor.java
+++ src/main/java/pl/training/shop/payments/PaymentProcessor.java
@@ -34,7 +34,8 @@ public class PaymentProcessor implements PaymentService {
     }
 
     private FastMoney calculatePaymentValue(FastMoney paymentValue) {
-        return paymentValue.add(paymentFeeCalculator.calculateFee(paymentValue));
+        var paymentFee = paymentFeeCalculator.calculateFee(paymentValue);
+        return paymentValue.add(paymentFee);
     }
 
     @Override

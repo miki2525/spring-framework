6665f72a5fc4b2fcbd5ac38dfc618b990ebebf52
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-16 12:55:15 +0100

    Base project

--- src/main/java/pl/training/shop/payments/adapters/persistence/PaymentEntity.java
+++ src/main/java/pl/training/shop/payments/adapters/persistence/PaymentEntity.java
@@ -4,6 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
@@ -17,6 +18,7 @@ public class PaymentEntity {
 
     @Id
     private String id;
+    @Column(name = "amount")
     private BigDecimal value;
     private String currency;
     private Instant timestamp;

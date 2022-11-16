fc261cc1fd0954d9c7c65b5dc2646991fc32d4a5
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-16 15:19:07 +0100

    Base project

--- src/main/java/pl/training/shop/payments/PaymentProcessor.java
+++ src/main/java/pl/training/shop/payments/PaymentProcessor.java
@@ -2,7 +2,7 @@ package pl.training.shop.payments;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
-import org.javamoney.moneta.FastMoney;
+import org.javamoney.moneta.Money;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -48,7 +48,7 @@ public class PaymentProcessor implements PaymentService {
         return paymentsRepository.save(payment);
     }
 
-    private Payment createPayment(FastMoney paymentValue) {
+    private Payment createPayment(Money paymentValue) {
         return Payment.builder()
                 .id(paymentIdGenerator.getNext())
                 .value(paymentValue)
@@ -57,7 +57,7 @@ public class PaymentProcessor implements PaymentService {
                 .build();
     }
 
-    private FastMoney calculatePaymentValue(FastMoney paymentValue) {
+    private Money calculatePaymentValue(Money paymentValue) {
         var paymentFee = paymentFeeCalculator.calculateFee(paymentValue);
         return paymentValue.add(paymentFee);
     }

5fe0b6368a316c070ba03859a995661a914b96d6
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-16 15:21:27 +0100

    Base project

--- src/main/java/pl/training/shop/Application.java
+++ src/main/java/pl/training/shop/Application.java
@@ -1,7 +1,7 @@
 package pl.training.shop;
 
 import lombok.extern.java.Log;
-import org.javamoney.moneta.FastMoney;
+import org.javamoney.moneta.Money;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.training.shop.payments.PaymentRequest;
 import pl.training.shop.payments.PaymentService;
@@ -15,7 +15,7 @@ public class Application {
     public static void main(String[] args) {
         try (var container = new ClassPathXmlApplicationContext(APPLICATION_CONFIGURATION)) {
             var paymentService = container.getBean(PaymentService.class);
-            var paymentRequest = new PaymentRequest(1L, FastMoney.of(1_000, DEFAULT_CURRENCY_CODE));
+            var paymentRequest = new PaymentRequest(1L, Money.of(1_000, DEFAULT_CURRENCY_CODE));
             var payment = paymentService.process(paymentRequest);
             log.info(payment.toString());
         }

da4b0db01b9c22e46faa75117b42b52182114736
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-16 10:31:13 +0100

    Base project

--- src/main/java/pl/training/shop/commons/money/FastMoneyMapper.java
+++ src/main/java/pl/training/shop/commons/money/FastMoneyMapper.java
@@ -4,19 +4,22 @@ import org.javamoney.moneta.FastMoney;
 import org.mapstruct.Mapper;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 @Mapper(componentModel = "spring")
 public interface FastMoneyMapper {
 
     default String toText(FastMoney money) {
-        return Optional.ofNullable(money)
-                .map(FastMoney::toString)
-                .orElseThrow(IllegalArgumentException::new);
+        return map(money, FastMoney::toString);
     }
 
     default FastMoney toMoney(String text) {
-        return Optional.ofNullable(text)
-                .map(FastMoney::parse)
+        return map(text, FastMoney::parse);
+    }
+
+    private <I, O> O map(I input, Function<I, O> mapper) {
+        return Optional.ofNullable(input)
+                .map(mapper)
                 .orElseThrow(IllegalArgumentException::new);
     }
 

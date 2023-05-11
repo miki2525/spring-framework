620ffe3686074907d32a99ea7718c9a5c2441a98
landrzejewski <landrzejewski.poczta@gmail.com>
2023-05-11 15:59:16 +0200

    rest

--- src/main/java/pl/training/shop/ApplicationConfiguration.java
+++ src/main/java/pl/training/shop/ApplicationConfiguration.java
@@ -1,88 +1,18 @@
 package pl.training.shop;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.training.shop.payments.adapters.time.SystemTimeProvider;
 import pl.training.shop.payments.ports.TimeProvider;
 
-import jakarta.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.Properties;
-
-@PropertySource("classpath:jdbc.properties")
-@EnableWebMvc
-@EnableJpaRepositories
-@EnableTransactionManagement
-@EnableAspectJAutoProxy
-@ComponentScan
 @Configuration
 public class ApplicationConfiguration implements WebMvcConfigurer {
 
     @Bean
     public TimeProvider systemTimeProvider() {
         return new SystemTimeProvider();
-    }
-
-    @Bean
-    public DataSource dataSource(Environment environment) {
-        var datasource = new HikariDataSource();
-        datasource.setUsername(environment.getProperty("database.username"));
-        datasource.setPassword(environment.getProperty("database.password"));
-        datasource.setJdbcUrl(environment.getProperty("database.url"));
-        datasource.setDriverClassName(environment.getProperty("database.driver"));
-        datasource.setMaximumPoolSize(20);
-        return datasource;
-    }
-
-    @Bean
-    public PropertiesFactoryBean jpaProperties() {
-        var factoryBean = new PropertiesFactoryBean();
-        factoryBean.setLocation(new ClassPathResource("jpa.properties"));
-        return factoryBean;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Properties jpaProperties) {
-        var factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setJpaProperties(jpaProperties);
-        factoryBean.setPackagesToScan("pl.training.shop");
-        factoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-        return factoryBean;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        var messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("exceptions");
-        return messageSource;
-    }
-
-    @Bean
-    public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
-        var factoryBean = new LocalValidatorFactoryBean();
-        factoryBean.setValidationMessageSource(messageSource);
-        return factoryBean;
     }
 
     @Override

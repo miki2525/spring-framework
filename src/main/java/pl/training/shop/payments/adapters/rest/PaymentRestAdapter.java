df3293aec62b074b42931266229cd90c3f797efb
landrzejewski <landrzejewski.poczta@gmail.com>
2023-05-11 13:24:02 +0200

    rest

--- src/main/java/pl/training/shop/payments/adapters/rest/PaymentRestAdapter.java
+++ src/main/java/pl/training/shop/payments/adapters/rest/PaymentRestAdapter.java
@@ -1,14 +1,35 @@
 package pl.training.shop.payments.adapters.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.training.shop.commons.web.LocationUri;
+import pl.training.shop.payments.ports.PaymentService;
 
+@RequestMapping("payments")
 @RestController
+@RequiredArgsConstructor
 public class PaymentRestAdapter {
 
-    @GetMapping("greetings")
-    public String sayHello() {
-        return "Hi";
+    private final PaymentService paymentService;
+    private final RestPaymentMapper mapper;
+
+    @GetMapping("{id}")
+    public ResponseEntity<PaymentDto> getById(@PathVariable String id) {
+        var paymentDomain = paymentService.getById(id);
+        var paymentDto = mapper.toDto(paymentDomain);
+        return ResponseEntity.ok(paymentDto);
     }
+
+   @PostMapping
+   public ResponseEntity<PaymentDto> process(@RequestBody PaymentRequestDto paymentRequestDto) {
+        var paymentRequest = mapper.toDomain(paymentRequestDto);
+        var paymentDomain = paymentService.process(paymentRequest);
+        var paymentDto = mapper.toDto(paymentDomain);
+        var locationUri = LocationUri.fromRequest(paymentDto.getId());
+        return ResponseEntity.created(locationUri)
+                .body(paymentDto);
+   }
+
 
 }

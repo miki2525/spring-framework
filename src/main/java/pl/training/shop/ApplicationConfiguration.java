7c122afa45cf8835fe56e136ac9831c2ee2825d1
landrzejewski <landrzejewski.poczta@gmail.com>
2023-05-12 08:44:58 +0200

    rest

--- src/main/java/pl/training/shop/ApplicationConfiguration.java
+++ src/main/java/pl/training/shop/ApplicationConfiguration.java
@@ -2,11 +2,13 @@ package pl.training.shop;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.training.shop.payments.adapters.time.SystemTimeProvider;
 import pl.training.shop.payments.ports.TimeProvider;
 
+//@Profile("devlopment")
 @Configuration
 public class ApplicationConfiguration implements WebMvcConfigurer {
 
